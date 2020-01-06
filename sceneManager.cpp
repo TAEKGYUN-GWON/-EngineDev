c869c947fb677023bafad766e236ba4c43042a17
leehn5 <31524982+leehn5@users.noreply.github.com>
2020-01-06 14:20:43 +0900

    씬매니저 수정

--- sceneManager.cpp
+++ sceneManager.cpp
@@ -68,7 +68,7 @@ HRESULT sceneManager::changeScene(string sceneName)
 	if (SUCCEEDED(find->second->init()))
 	{
 		//어떤 씬의 정보가 처음에 들어있기 때문에 릴리즈 시켜줘라
-		//if (_currentScene) _currentScene->release();
+		if (_currentScene) _currentScene->release();
 
 		//현재 씬에 바꾸려는 씬을 담는다
 		_currentScene = find->second;
